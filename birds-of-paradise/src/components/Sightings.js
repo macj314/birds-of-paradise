371e5ccf64b32da3a14a0eb27a4973dd802731ba
unknown <macjay314@gmail.com>
2020-07-24 16:52:17 -0700

    Experiment with bar graph rendering.

--- birds-of-paradise/src/components/Sightings.js
+++ birds-of-paradise/src/components/Sightings.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { fetchKingCounty } from '../actions/index';
-import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, VerticalBarSeries} from 'react-vis';
 // import cx from 'classnames';
 // import "~react-vis/dist/style";
 
@@ -25,31 +25,29 @@ class Sightings extends React.Component {
       console.log(sightings);
       return (
         <React.Fragment>
-          <XYPlot
-            width={300}
-            height={300}>
-            <HorizontalGridLines />
-            <LineSeries
-              data={[
-                {x: 1, y: 10},
-                {x: 2, y: 5},
-                {x: 3, y: 15}
-              ]}/>
-            <XAxis />
-            <YAxis />
-          </XYPlot>
           <h2>Sightings in King County</h2>
           <hr/>
-          <ol>
+          
             {sightings.map((sighting, index) =>
               <li key={index}>
+                <XYPlot
+                  width={300}
+                  height={300}>
+                  <VerticalBarSeries
+                    data={[
+                      {x: 'A', y: 10},
+                      {x: 'B', y: 5},
+                      {x: 'C', y: 15}
+                    ]}/>
+                  <XAxis />
+                  <YAxis />
+                </XYPlot>
                 <h3>Common Name: {sighting.comName}</h3>
                 <h3>Sighting Location: {sighting.locName}</h3>
                 <h4>Date: {sighting.obsDt}</h4>
                 <h4>Number: {sighting.howMany}</h4>
               </li>
             )}
-          </ol>
         </React.Fragment>
       );
     }
