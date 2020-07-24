9c21e2bc1a68246e26cd9c82d76a61d1b3a24c6d
unknown <macjay314@gmail.com>
2020-07-24 09:46:55 -0700

    Add an api call for sightings by region.

--- birds-of-paradise/src/actions/index.js
+++ birds-of-paradise/src/actions/index.js
@@ -13,3 +13,16 @@ export const getSightingsFailure = (error) => ({
   type: c.GET_SIGHTINGS_FAILURE,
   error
 });
+
+export const sightingsByRegion = () => {
+  return dispatch => {
+    dispatch(requestPlaces);
+    return fetch('https://api.ebird.org/v2/data/obs/US-WA-033/recent', {headers: {"X-eBirdApiToken": `${process.env.REACT_APP_API_KEY}`}})
+    .then(response => response.json())
+    .then(
+      (jsonifiedResponse) => {dispatch(getPlacesSuccess(jsonifiedResponse.results));})
+    .catch((error) => {
+      dispatch(getPlacesFailure(error));
+    });
+  }
+}
