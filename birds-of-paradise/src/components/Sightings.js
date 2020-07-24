b5a55fa4eeabb0d4d31e758b69c19025662aac92
unknown <macjay314@gmail.com>
2020-07-24 14:58:27 -0700

    Attempt to organize data/styling.

--- birds-of-paradise/src/components/Sightings.js
+++ birds-of-paradise/src/components/Sightings.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from 'react-redux';
 import { fetchKingCounty } from '../actions/index';
+// import "~react-vis/dist/style";
 
 class Sightings extends React.Component {
   constructor(props) {
