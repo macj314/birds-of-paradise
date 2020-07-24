e2578d27e648147beca6a229a9d08966cb00ebdc
unknown <macjay314@gmail.com>
2020-07-24 09:59:48 -0700

    Start creating render().

--- birds-of-paradise/src/components/Sightings.js
+++ birds-of-paradise/src/components/Sightings.js
@@ -1,25 +1,18 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { sightingsByRegion } from '../actions/index';
 
-function Header() {
-  const headerStyles = {
-    backgroundColor: 'black', //#000000
-    border: 'solid 1pt grey',
-    textAlign: "center",
-    borderRadius: "8px",
-    marginBottom: "20px",
-    color: 'white'
+class Sightings extends React.Component {
+  constructor(props) {
+    super(props);
   }
-  return (
-    <React.Fragment>
-      <div style={headerStyles}>
-      <h1>HEADER Header header</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio harum sunt dolorem est nobis eaque libero aliquid deleniti. Pariatur possimus quis quam obcaecati dolor saepe delectus, in doloribus animi! Dolorum!</p>
-      </div>
-      {/* <Image src={catDonut}>
-        <Transformation width="250" height="250" gravity="faces" crop="fill" />
-      </Image> */}
-    </React.Fragment>
-  )
-}
 
-export default Header;
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(sightingsByRegion());
+  }
+
+  render () {
+    
+  }
+}
